e5ae41f2750459cacaab4e8583c83d4bda6f48e8
JanbolotSabyrbekov <sabyrbekovjanbolot@gmail.com>
2020-08-15 22:44:41 +0600

    Add src

--- app/index.php
+++ app/index.php
@@ -3,8 +3,11 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
+	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 	
+	<script src="./js/jsLibs/jsLibs.min.js"></script>
+	<script src="./js/main.js"></script>
 </body>
 </html>
---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+
+gulp.task('sass', function(){
+	return gulp.src('../articlesAndAboutMe/sass/**/*.sass')
+		.pipe(sass({outputStyle: 'compressed'}))
+		.pipe(gulp.dest('app/css'))
+		.pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('js', function(){
+	return gulp.src('app/js/*.js')
+	  .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('jsLibs', function(){
+	return gulp.src([
+		'node_modules/jquery/dist/jquery.js',
+		'node_modules/bootstrap/dist/js/bootstrap.js'
+	])
+		.pipe(concat('jsLibs.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('app/js/jsLibs'))
+});
+
+gulp.task('php', function(){
+	return gulp.src('app/**/*.php')
+		.pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('watch', function(){
+	gulp.watch('../articlesAndAboutMe/sass/**/*.sass', gulp.parallel('sass'));
+	gulp.watch('app/js/*.js', gulp.parallel('js'));
+	gulp.watch('app/**/*.php', gulp.parallel('php'));
+});
+
+gulp.task('browserSync', function(){
+	browserSync.init({
+		proxy: 'petProject/articlesAndAboutMe/app'
+	})
+});
+
+gulp.task('default', gulp.parallel('sass', 'js', 'jsLibs', 'php', 'watch', 'browserSync'))
