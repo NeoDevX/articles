03387f042b1bf2754c84e372b5eafb15cf90c084
JanbolotSabyrbekov <sabyrbekovjanbolot@gmail.com>
2020-08-24 17:55:01 +0600

    Add changes in files db/config.php

---
+++ app/includes/db/config.php
@@ -0,0 +1,16 @@
+<?php 
+
+	$config = array(
+		'title' => 'My_Blog',
+		'vk_url' => 'https://connect.vk.com/account/#/personal',
+		'db' => array(
+			'server' => 'localhost',
+			'username' => 'root',
+			'password' => 'root',
+			'name' => 'myblog_db'
+		)
+	);
+
+	include_once '../db/db.php';
+
+?>
---
+++ app/includes/db/db.php
@@ -0,0 +1,19 @@
+<?php 
+	
+	require_once 'config.php'
+
+	$connection = array(
+		$config['db']['server'],
+		$config['db']['username'],
+		$config['db']['password'],
+		$config['db']['name']
+	);
+
+	if (!$connection)
+	{
+		echo "Не удалось подключиться к базе данных :( !<br>";
+		echo mysqli_connect_error();
+		exit();
+	}
+
+?>
